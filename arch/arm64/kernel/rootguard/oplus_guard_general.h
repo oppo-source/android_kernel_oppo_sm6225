85db296e9cf2d0d1f110f250d1e3c60a8654fbc5
pswbuild <pswbuild@oppo.com>
2022-07-23 10:08:17 +0800

    Synchronize codes for Oppo Pad Air

---
+++ arch/arm64/kernel/rootguard/oplus_guard_general.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+* Copyright (c)  2008- 2020  Oplus. All rights reserved..
+*/
+#ifndef OPLUS_GUARD_GENERAL_H_
+#define OPLUS_GUARD_GENERAL_H_
+
+bool is_unlocked(void);
+
+bool is_normal_boot_mode(void);
+
+#endif /*OPLUS_GUARD_GENERAL_H_*/
